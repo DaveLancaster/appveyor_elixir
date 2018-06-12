7e8d00ddc7d3e62aaede7de23209c23bd96ae782
dave <lancaster.dave@gmail.com>
2018-06-12 10:27:02 +0100

    include endpoints module

---
+++ lib/appveyor/endpoints.ex
@@ -0,0 +1,21 @@
+defmodule Appveyor.Endpoints do
+  def collaborators do 
+    "/collaborators"
+  end
+
+  def environments do
+    "/environments"
+  end
+
+  def projects do
+    "/projects"
+  end
+
+  def users do
+    "/users"
+  end
+
+  def roles do
+    "/roles"
+  end
+end
